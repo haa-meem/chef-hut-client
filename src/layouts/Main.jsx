87f3b4e0abb96fd584de60e6b32b3a5d61771759
Haa-meem <haameemalhamra@gmail.com>
2023-05-03 10:20:53 +0600

    react bootstrapinstalled and folder structure updated

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../pages/shared/Header/Header';
+import Footer from '../pages/shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/pages/shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center'><small>copyright of Chef Recipe Hunter</small></p>
+        </div>
+    );
+};
+
+export default Footer;
