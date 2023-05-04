dc10cbe79cfadbbeebb18bcac9528e0157c3a945
Haa-meem <haameemalhamra@gmail.com>
2023-05-04 17:16:56 +0600

    404 page not found

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import pic from '../assets/404-error-page-not-found.jpg'
+const NotFound = () => {
+    return (
+        <div>
+            <img src={pic} alt="" width='100%' />
+        </div>
+    );
+};
+
+export default NotFound;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,40 +7,46 @@ import Login from "../pages/Login/Login/Login";
 import Register from "../pages/Login/Register/Register";
 import Blog from "../pages/Blog/Blog";
 import PrivateRoute from "./PrivateRoute";
+import NotFound from "../pages/NotFound";
 
-const router=createBrowserRouter([
+const router = createBrowserRouter([
     {
-        path:'/',
-        element:<Main></Main>,
-        children:[
+        path: '/',
+        element: <Main></Main>,
+        children: [
             {
-                path:'/',
-                element:<Home></Home>
+                path: '/',
+                element: <Home></Home>
             },
             {
-                path:'/login',
-                element:<Login></Login>
+                path: '/login',
+                element: <Login></Login>
             },
             {
-                path:'/register',
-                element:<Register></Register>
+                path: '/register',
+                element: <Register></Register>
             },
             {
-                path:'/blog',
-                element:<Blog></Blog>
+                path: '/blog',
+                element: <Blog></Blog>
             }
         ]
     },
     {
-        path:'chef',
-        element:<ChefLayout></ChefLayout>,
-        children:[
+        path: 'chef',
+        element: <ChefLayout></ChefLayout>,
+        children: [
             {
-                path:':id',
-                element:<PrivateRoute><Chef></Chef></PrivateRoute>,
-                loader:({params})=>fetch(`http://localhost:5000/chefs/${params.id}`)
+                path: ':id',
+                element: <PrivateRoute><Chef></Chef></PrivateRoute>,
+                loader: ({ params }) => fetch(`http://localhost:5000/chefs/${params.id}`)
             }
         ]
+    },
+
+    {
+        path: '*',
+        element: <NotFound></NotFound>
     }
 ])
 
